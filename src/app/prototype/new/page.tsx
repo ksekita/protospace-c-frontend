d30b111e58adb1530f241ba004182c626910139e
kaitooki-wq <kaito.oki@infomart.co.jp>
2026-07-21 12:03:32 +0900

    新規投稿ページのCSS

--- src/app/prototype/new/page.tsx
+++ src/app/prototype/new/page.tsx
@@ -1,30 +1,61 @@
+import styles from "./page.module.css";
+
 export default function NewPrototypePage() {
   return (
-    <main className="container">
-      <h1>新規プロトタイプ投稿</h1>
+    <main className={styles.container}>
+      <h2 className={styles.title}>新規プロトタイプ投稿</h2>
 
-      <form>
-        <div>
-          <label htmlFor="title">プロトタイプの名称</label>
-          <input type="text" id="title" name="title" />
+      <form
+        method="post"
+        action="/api/prototypes"
+        encType="multipart/form-data"
+      >
+        <div className={styles.formGroup}>
+          <label htmlFor="title" className={styles.label}>
+            プロトタイプの名称
+          </label>
+          <input type="text" id="title" name="title" className={styles.input} />
         </div>
 
-        <div>
-          <label htmlFor="catchcopy">キャッチコピー</label>
-          <input type="text" id="catchphrase" name="catchphrase" />
+        <div className={styles.formGroup}>
+          <label htmlFor="catchphrase" className={styles.label}>
+            キャッチコピー
+          </label>
+          <textarea
+            id="catchphrase"
+            name="catchphrase"
+            rows={3}
+            className={styles.textarea}
+          />
         </div>
 
-        <div>
-          <label htmlFor="concept">コンセプト</label>
-          <textarea id="concept" name="concept" rows={4} />
+        <div className={styles.formGroup}>
+          <label htmlFor="concept" className={styles.label}>
+            コンセプト
+          </label>
+          <textarea
+            id="concept"
+            name="concept"
+            rows={4}
+            className={styles.textarea}
+          />
         </div>
 
-        <div>
-          <label htmlFor="image">プロトタイプの画像</label>
-          <input type="file" id="protoimage" name="protoimage" />
+        <div className={styles.formGroup}>
+          <label htmlFor="image" className={styles.label}>
+            プロトタイプの画像
+          </label>
+          <input
+            type="file"
+            id="image"
+            name="image"
+            className={styles.fileInput}
+          />
         </div>
 
-        <button type="submit">保存する</button>
+        <button type="submit" className={styles.submitBtn}>
+          保存する
+        </button>
       </form>
     </main>
   );
