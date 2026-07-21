ac4c09192c63da717480632b6c88d92cd3dab193
akira <a.ono@infomart.co.jp>
2026-07-21 06:53:12 +0000

    login時のactionを記述

--- src/lib/actions/authActions.ts
+++ src/lib/actions/authActions.ts
@@ -1,7 +1,8 @@
 "use server";
 
 import { redirect } from "next/navigation";
-import api from "../api/apiClient";
+// import api from "../api/apiClient";
+import { cookies } from "next/headers";
 
 /**
  * backendに送るもの
@@ -32,13 +33,31 @@ import api from "../api/apiClient";
  *  position
  */
 
+const mockData = {
+  email: "user@example.com",
+  password: "password",
+};
+
 export async function loginAction(prevState: null, formData: FormData) {
   //  emailとpasswordを取得
   const email = formData.get("email") as string;
   const password = formData.get("password") as string;
 
   try {
-    const response = await api.post("/auth/login", { email, password });
+    // const response = await api.post("/auth/login", { email, password });
     // await createSession
-  } catch (error) {}
+
+    if (email === mockData.email && password === mockData.password) {
+      const cookieStore = await cookies();
+      cookieStore.set("session_token", "dummy-jwt-token-abc123", {
+        httpOnly: true,
+      });
+    } else {
+      return { error: "メールアドレスまたはパスワードが違います" };
+    }
+  } catch (error) {
+    console.log("error", error);
+    return { error: "通信エラーが発生しました" };
+  }
+  redirect("/");
 }
