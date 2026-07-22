84eb5eb7c037a1834cdd50ac6c594228f31b89b1
akira <a.ono@infomart.co.jp>
2026-07-22 01:10:43 +0000

    テストの時のパスのエイリアスを設定

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from "vitest/config";
+import path from "path";
 
 export default defineConfig({
   test: {
     environment: "jsdom",
     globals: true,
     setupFiles: "./vitest.setup.ts", // テスト実行前に読み込むファイルを指定
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
