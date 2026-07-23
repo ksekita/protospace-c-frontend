16bc9d1459aa0a85531076b1c095cbd38155f062
akira <a.ono@infomart.co.jp>
2026-07-23 00:29:51 +0000

    ルート制限を設定

--- proxy.ts
+++ proxy.ts
@@ -1,15 +1,9 @@
 // proxy.ts
 import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { NextRequest } from "next/server";
 
 // 未ログイン時でも見れる画面のリスト
-const PUBLIC_ROUTES = [
-  "/auth/login",
-  "/auth/register",
-  "/",
-  "/prototype",
-  "/prototype/detail",
-];
+const PUBLIC_ROUTES = ["/auth/login", "/auth/register", "/", "/prototype"];
 
 export function proxy(request: NextRequest) {
   // リクエストしてるページのurl
@@ -21,14 +15,27 @@ export function proxy(request: NextRequest) {
   if (token && (pathname === "/auth/login" || pathname === "/auth/register")) {
     return NextResponse.redirect(new URL("/", request.url));
   }
+  // prototypeの中ではじくものを決める
+  const isPrototypeDetail =
+    pathname.startsWith("/prototype/") &&
+    pathname !== "/prototype/new" &&
+    !pathname.endsWith("/edit"); //
+
+  const isUserPage =
+    pathname.startsWith("/user/") && !pathname.endsWith("/edit");
 
   // 未ログイン時リストに入ってたらクリア
-  if (PUBLIC_ROUTES.includes(pathname)) {
+  if (PUBLIC_ROUTES.includes(pathname) || isPrototypeDetail || isUserPage) {
     return NextResponse.next();
   }
 
   // トークンが無い（未ログイン）状態で、保護されたページにアクセスしようとした場合
   if (!token) {
+    // 新規投稿ページは未ログイン時のuserはホーム画面に弾く
+    if (pathname === "/prototype/new" || pathname.endsWith("/edit")) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+
     // ログイン画面（/auth/login）へ強制リダイレクト
     return NextResponse.redirect(new URL("/auth/login", request.url));
   }
