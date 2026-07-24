e893a69c2b3d4af6bd2de3d37e4d44840ccb20c1
akira <a.ono@infomart.co.jp>
2026-07-24 01:42:13 +0000

    ユーザー詳細のページを作成

---
+++ src/app/users/{id}/page.tsx
@@ -0,0 +1,28 @@
+import PrototypeList from "@/components/users/detail/PrototypeList";
+import Detail from "@/components/users/detail/Detail";
+import styles from "./UserDetail.module.css";
+import { userDetail } from "@/lib/api/userDetail";
+
+export default async function UserDetail({
+  params,
+}: {
+  params: Promise<{ id: number }>;
+}) {
+  const userId = (await params).id;
+  const response = await userDetail(userId);
+  // notfoudページ後日実装
+  //   if (!response) return notFound;
+
+  // 後で消す
+  console.log("詳細データ:", response);
+
+  return (
+    <div className="innner">
+      <div className={styles.user_wrapper}>
+        {/* このrespomse.userDetailとprototypeListは、受け取る変数はbackendによって変化する */}
+        <Detail user={response.userDetail} />
+        <PrototypeList prototypes={[response.prototypeList]} />
+      </div>
+    </div>
+  );
+}
