2b1c22699840516ddd58df00b9ee3a3b3b9e57d7
kaitooki-wq <kaito.oki@infomart.co.jp>
2026-07-24 06:00:16 +0000

    Reapply "投稿編集ページ　実装　テスト"
    This reverts commit f18ae11b5757289dcde24d86b16f609675abd911.

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -4,7 +4,7 @@ import styles from "../auth.module.css";
 export default function LoginPage() {
   return (
     <div className="inner">
-      <h2 className={styles.page_heading}>ユーザーログイン</h2>
+      <h2 className={styles.page_heading}>ユーザー口グイン</h2>
       <Login />
     </div>
   );
