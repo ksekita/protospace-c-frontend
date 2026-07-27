05733539ed7e35badddd45155832439b0178a780
akira <a.ono@infomart.co.jp>
2026-07-27 04:35:50 +0000

    secretKey存在チェック

--- src/lib/utils/auth.ts
+++ src/lib/utils/auth.ts
@@ -5,11 +5,17 @@ export async function isTokenValid(
   token: string | undefined,
 ): Promise<boolean> {
   if (!token) return false;
-  try {
-    const secret = new TextEncoder().encode(process.env.JWT_SECRET);
-    await jwtVerify(token, secret);
-    return true;
-  } catch {
+  const secretKey = process.env.JWT_SECRET;
+  if (!secretKey) {
+    console.error("JWT_SECRETが設定されていません");
     return false;
+  } else {
+    try {
+      const secret = new TextEncoder().encode(secretKey);
+      await jwtVerify(token, secret);
+      return true;
+    } catch {
+      return false;
+    }
   }
 }
