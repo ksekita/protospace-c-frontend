4b9c17016ac2f9b10f8570ed9f706ce380663af9
akira <a.ono@infomart.co.jp>
2026-07-28 02:53:25 +0000

    imageを取得するために修正

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,24 @@
 import type { NextConfig } from "next";
 
+const backendHost = process.env.BACKEND_HOST_NAME || "placeholder.onrender.com";
+
 const nextConfig: NextConfig = {
   /* config options here */
-  // images: {
-  //   remotePatterns: [
-  //     {
-  //       protocol: "http",
-  //       hostname: "localhost",
-  //       port: "8080",
-  //       pathname: "/images/**",
-  //     },
-  //   ],
-  // },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "8080",
+        pathname: "/images/**",
+      },
+      {
+        protocol: "https",
+        hostname: backendHost,
+        pathname: "/images/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
