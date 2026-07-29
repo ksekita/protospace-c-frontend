c4b6032cf912aa5933a6769e58251dbd128a6835
ksekita <k.sekita@infomart.co.jp>
2026-07-29 15:38:16 +0900

    fix-allview-function

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      // ローカル環境
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "8080",
+        pathname: "/images/**",
+      },
+      // 本番環境
+      {
+        protocol: "https",
+        hostname: "*.onrender.com",
+        pathname: "/images/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
