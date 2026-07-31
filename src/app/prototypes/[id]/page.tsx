fd853ac5a1e0c62a982cdeb0bccadea0ad4425e4
kaitooki-wq <kaito.oki@infomart.co.jp>
2026-07-31 02:18:52 +0000

    消えたページの復元

---
+++ src/app/prototypes/[id]/page.tsx
@@ -0,0 +1,40 @@
+import PrototypeDetail from "@/components/prototype/detail/PrototypeDetail";
+import styles from "./PrototypeDetailPage.module.css";
+import CommentForm from "@/components/prototype/detail/CommentForm";
+import CommentList from "@/components/prototype/detail/CommentList";
+import { isTokenValid } from "@/lib/utils/auth";
+import { cookies } from "next/headers";
+import { prototypeDetail } from "@/lib/api/prototypeDetail";
+import { allCommentList } from "@/lib/api/commentList";
+import { userInfo } from "@/lib/api/useGetPrototype";
+
+export default async function PrototypeDetailPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  const prototypeId = Number(id);
+
+  // コメントフォーム表示
+  const cookieStore = await cookies();
+  const token = cookieStore.get("jwt_token")?.value;
+  const isLoggedIn = await isTokenValid(token);
+
+  //  投稿詳細
+  const detail = await prototypeDetail(prototypeId);
+  const user = await userInfo();
+
+  // コメント一覧
+  const comment = await allCommentList(prototypeId);
+
+  return (
+    <div className="inner">
+      <PrototypeDetail prototypeDetail={detail} userId={user.id} />
+      <div className={styles.prototype_comments}>
+        <CommentForm isLoggedIn={isLoggedIn} prototypeId={prototypeId} />
+        <CommentList commentList={comment} />
+      </div>
+    </div>
+  );
+}
