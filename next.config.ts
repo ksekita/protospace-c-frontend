df1711aad24ce76a3b4e6c26098214a15042b20c
akira <a.ono@infomart.co.jp>
2026-07-31 06:27:16 +0000

    使ってなかったので削除

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,5 @@
 import type { NextConfig } from "next";
 
-const backendHost = process.env.BACKEND_HOST_NAME || "placeholder.onrender.com";
-
 const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
