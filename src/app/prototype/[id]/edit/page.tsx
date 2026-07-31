4e1fd9244a958b11fce8cd172281f0a981d46272
akira <a.ono@infomart.co.jp>
2026-07-31 08:35:18 +0000

    あああ

--- src/app/prototype/[id]/edit/page.tsx
+++ src/app/prototype/[id]/edit/page.tsx
@@ -3,6 +3,7 @@ import styles from "./edit.module.css";
 import Editprototype from "@/components/prototype/editPrototype/Editprototype";
 import { userInfo } from "@/lib/api/useGetPrototype";
 import { redirect } from "next/navigation";
+import { prototypeDetail } from "@/lib/api/prototypeDetail";
 
 export default async function EditPrototypePage({
   params,
@@ -12,10 +13,11 @@ export default async function EditPrototypePage({
   const { id } = await params;
   const prototypeId = Number(id);
   const response = await api.get(`prototypes/${prototypeId}`);
+  const protoTypeUserId = await prototypeDetail(prototypeId);
   const user = await userInfo();
   const prototypeData = response.data;
 
-  if (user.id !== prototypeId) {
+  if (user.id !== protoTypeUserId.userId) {
     redirect(`/prototype/${prototypeId}`);
   }
 
